463f75a43212c34c7f50c72400bdc7828d95934e
Marko Marijan <markomar543@gmail.com>
2024-10-20 18:04:52 +0200

    Dodan zadatak 5

---
+++ vjezba_5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Vjezba 4</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Marko Marijan">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+    <body>
+        <?php
+                $rand_num = rand(1,9);
+                
+            print '
+            <h1>Pogodi broj!</h1>
+            <form action="" method="post" id="calculator">
+                <div class="form-group">
+                    <label for="bum">Upiši broj od 1 do 9*</label>
+                    <input type="number" name="num" id="num" required="required" value="num">
+                </div>';
+
+                if (isset($_POST['num'])) {
+                    if ($_POST['num'] == $rand_num) {
+                        print'<button> Pogodak, probaj ponovo! </button>';
+                    } else {
+                        print '<button> Krivo, proaj ponovo!</button>';
+                    }
+                    print '<br>Zamišljeni broj je ' . $rand_num;
+                }
+               
+            print '</form>';
+        ?>
+    </body>
+</html>
