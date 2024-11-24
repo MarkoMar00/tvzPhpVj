8742fa3ddc2045d81ccdec4e3e6cd889d7ee3a17
Marko Marijan <markomar543@gmail.com>
2024-11-24 20:21:08 +0100

    Dodan zadatak 9

---
+++ vjezba_9.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+    <html lang='hr'>
+        <head>
+            <title>Ducan radno vrijeme</title>  
+            <meta http-equiv='content-type' content='text/html; charset=UTF-8>
+            <meta name='description' content=''>
+            <meta name='keywords' content=''>
+            <meta name='author' content='Marko Marijan'>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+        </head> 
+
+        <body>
+            <h1>Stanje dućana</h1>
+        <?php
+            function stanjeDucana() {
+                $h = date("H");
+                $d = date("D");
+
+                if ($d != "Sat" && $d != "Sun"){
+                    if ($h >= 8 && $h <= 20) {
+                        ducan();
+                    } else {
+                        ducan("zatvoren");
+                    }
+                } elseif ($d == "Sat") {
+                    if ($h >= 9 && $h <= 14) {
+                        ducan();
+                    } else {
+                        ducan("zatvoren");
+                    }
+                } else {
+                    ducan("zatvoren");
+                }
+            }
+
+            function ducan($stanje="otvoren"){
+                echo "Ducan je $stanje";
+            }
+        ?>
+        
+        <h2>Trentuno stanje:</h2>
+        <?php
+            stanjeDucana();
+        ?>
+
+        </body>
+    </html>
